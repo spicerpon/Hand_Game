3979fca604ab90d2cb08b7883a98796a0097bae4
spicerpon <spicerpon@gmail.com>
2016-12-14 19:12:16 +0900

    업데이트

---
+++ Hand_Game/Assets/Scripts/Cat_AI.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cat_AI : MonoBehaviour {
+
+	Animation anim;
+
+	public AnimationClip idleAnimation;
+	public AnimationClip walkAnimation;
+	public AnimationClip runAnimation;
+	public AnimationClip jumpPoseAnimation;
+
+	public float walkMaxAnimationSpeed = 0.75f;
+	public float trotMaxAnimationSpeed = 1.0f;
+	public float runMaxAnimationSpeed = 1.0f;
+	public float jumpAnimationSpeed = 1.15f;
+	public float landAnimationSpeed = 1.0f;
+
+	enum CharacterState {
+		Idle = 0,
+		Walking = 1,
+		Trotting = 2,
+		Running = 3,
+		Jumping = 4,
+	}
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animation> ();
+
+		anim.AddClip (idleAnimation, "Idle");
+		anim.AddClip (walkAnimation, "Walk");
+		anim.AddClip (runAnimation, "Run");
+		anim.AddClip (jumpPoseAnimation, "Jump");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//anim.CrossFade ("Run");
+	
+	}
+}
---
+++ Hand_Game/Assets/Standard Assets/Character Controllers/Sources/Scripts/Camera_Move.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_Move : MonoBehaviour {
+
+	public float moveSpeed = 5f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+
+		transform.Translate (0f, 0f, h * moveSpeed * Time.deltaTime);
+
+	}
+	
+
+}
